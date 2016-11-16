332966395b98d9aef7e99402b22de259faf56f01
Aaron Trostle <aaron.trostle@gmail.com>
2016-11-15 21:47:32 -0800

    Add Header component, update index with Header, add about page

---
+++ components/header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Link from 'next/Link';
+
+export default () => (
+  <div id="header">
+    <Link href="/">Home</Link>
+    <Link href="/about">About</Link>
+  </div>
+);
--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,9 @@
 import React from 'react';
-
 import Header from '../components/header';
 
 export default () => (
   <div>
     <Header />
-    <div>
-      Welcome to aarontrostle.com!
-    </div>
+    <div>About</div>
   </div>
 );
